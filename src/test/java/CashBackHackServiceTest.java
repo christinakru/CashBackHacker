1613964a059223d1fbaf6848acd5fdc59175f1af
Kristina Krutova <fruktovaya99@mail.ru>
2023-03-05 23:41:50 +0300

    Change to JUnit4

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class CashBackHackServiceTest {
+import org.example.CashbackHackService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+    @Test
+    public void cashback1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cashback500() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(500);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cashbackMore1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cashback0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+    }
 }
